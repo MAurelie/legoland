a2c8faf4034826da6f353b1056a996d9b2350fe7
MAurelie <martinez.aurelie.cf@gmail.com>
2019-09-11 14:23:51 +0200

    add TestBoiteLego and correct BoiteLego

---
+++ src/test/java/lego/test/TestBoiteLego.java
@@ -0,0 +1,27 @@
+package lego.test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import lego.BoiteLego;
+
+public class TestBoiteLego {
+
+	@Test
+	public void testConstructor() {
+		//given
+		var number = 21318;
+		var name = "La cabane dans l'arbre";
+		//when
+		var boite = new BoiteLego(number, name);
+		//then
+		assertAll(
+				() -> assertEquals(number, boite.getNumber(), "number"),
+				() -> assertEquals(name, boite.getNumber(), "name")
+			);
+	}
+
+}
