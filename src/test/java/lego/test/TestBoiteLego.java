7ff051a4beb9565f7a38e8d149437a42db51dd1a
MAurelie <martinez.aurelie.cf@gmail.com>
2019-09-12 09:58:23 +0200

    complete contructeur TestBoite

--- src/test/java/lego/test/TestBoiteLego.java
+++ src/test/java/lego/test/TestBoiteLego.java
@@ -15,12 +15,18 @@ public class TestBoiteLego {
 		//given
 		var number = 21318;
 		var name = "La cabane dans l'arbre";
+		var pieces = 3036;
+		var figurines = 4;
+		var price = 199.99;
 		//when
-		var boite = new Boite(number, name);
+		var boite = new Boite(number, name, pieces, figurines, price);
 		//then
 		assertAll(
 				() -> assertEquals(number, boite.getNumber(), "number"),
-				() -> assertEquals(name, boite.getName(), "name")
+				() -> assertEquals(name, boite.getName(), "name"),
+				() -> assertEquals(pieces, boite.getPiece(), "piece"),
+				() -> assertEquals(figurines, boite.getFigurines(), "figurines"),
+				() -> assertEquals(price, boite.getPrice(), "price")
 			);
 	}
 
