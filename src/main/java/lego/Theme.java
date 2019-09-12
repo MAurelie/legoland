91d1d2121c93619e3eb24246151ee3104668d47d
MAurelie <martinez.aurelie.cf@gmail.com>
2019-09-12 11:37:10 +0200

    Add class Theme

--- src/main/java/lego/Theme.java
+++ src/main/java/lego/Theme.java
@@ -1,5 +1,43 @@
 package lego;
 
-public class Theme {
+import java.util.Objects;
 
+public class Theme {
+	private String name;
+	
+	public Theme(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Le thème est : " + this.name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Theme other= (Theme) obj;
+
+		return this.name == other.name;
+}
 }
