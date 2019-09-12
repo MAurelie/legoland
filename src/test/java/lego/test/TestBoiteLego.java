8eaf234d6a88963670b2f3e63ea4ace94366e1cf
MAurelie <martinez.aurelie.cf@gmail.com>
2019-09-12 09:43:56 +0200

    update pom and src

--- src/test/java/lego/test/TestBoiteLego.java
+++ src/test/java/lego/test/TestBoiteLego.java
@@ -1,10 +1,10 @@
 package lego.test;
 
-import static org.junit.Assert.*;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import lego.Boite;
 
