1febcdc00e5ce1d9ea4b042dd39b5938d0a8141e
Khalil Ciret <REEEE>
2019-09-12 10:11:44 +0200

    j'avais fait de la merde mais là c'est bon lol

--- src/main/java/lego/Boite.java
+++ src/main/java/lego/Boite.java
@@ -5,18 +5,21 @@ public class Boite implements Comparable<Boite>
 {
 	private int number;
 	private String name;
-	
+
 	private int pieces;
 
 
 	private int figurines;
 	private double price;
 
-	
+
 	public Boite(int number, String name, int pieces, int figurines, double price) {
 		super();
 		this.number = number;
 		this.name = name;
+		this.pieces = pieces;
+		this.figurines = figurines;
+		this.price= price;
 	}
 	public int getNumber() {
 		return number;
@@ -30,13 +33,13 @@ public class Boite implements Comparable<Boite>
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Voici une boîte de lego \""+name+"\" (id = "+number+") contenant "+pieces+" pièces, avec "+figurines+" figurines. Elle coute "+price+"€.";
 	}
-	
-	
+
+
 	public int getPieces() {
 		return pieces;
 	}
@@ -60,7 +63,7 @@ public class Boite implements Comparable<Boite>
 		return 0;
 
 	}
-	
-	
-	
+
+
+
 }
