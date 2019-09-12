c00de479736327b6e82f4e8b927acd9312575364
Khalil Ciret <REEEE>
2019-09-12 11:41:02 +0200

    M'sieur on fait quoi quand on a fini?

--- src/test/java/lego/test/TestTheme.java
+++ src/test/java/lego/test/TestTheme.java
@@ -1,14 +1,39 @@
 package lego.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import lego.Theme;
+
 public class TestTheme {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testName() {
+		String ow = "Overwatch";
+		Theme owt = new Theme(ow);
+		assertTrue(ow == owt.getName());
 	}
+	
+	@Test
+	public void testEquals() {
+		String owName = "Overwatch";
+		Theme ow = new Theme(owName);
+		Theme ow2 = new Theme(owName);
+		assertTrue(ow.equals(ow2));
+	}
+	
+	@Test
+	public void testString() {
+		Theme ow = new Theme("Overwatch");
+		System.out.println(ow.toString());
+		assertEquals(ow.toString(),"Le thème est : Overwatch");
+	}
+	
+	
+	
 
 }
