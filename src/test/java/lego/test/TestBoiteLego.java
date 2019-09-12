b7fa8f9d9c70f253e970c9a17d567aa87df9d3a1
MAurelie <martinez.aurelie.cf@gmail.com>
2019-09-12 10:59:34 +0200

    Add TestCompareTo

--- src/test/java/lego/test/TestBoiteLego.java
+++ src/test/java/lego/test/TestBoiteLego.java
@@ -3,6 +3,7 @@ package lego.test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,4 +31,34 @@ public class TestBoiteLego {
 			);
 	}
 
+	@Test
+	public void testCompareTo() {
+		//given
+		var numberA = 21318;
+		var nameA = "La cabane dans l'arbre";
+		var piecesA = 3036;
+		var figurinesA = 4;
+		var priceA = 199.99;
+				
+		var numberB = 21317;
+		var nameB = "Steamboat Willie";
+		var piecesB = 751;
+		var figurinesB = 2;
+		var priceB = 89.99;
+		
+		var numberC = 75192;
+		var nameC = "Millennium Falcon";
+		var piecesC = 7541;
+		var figurinesC = 11;
+		var priceC = 799.99;
+		
+		//when
+		var boiteA = new Boite(numberA, nameA, piecesA, figurinesA, priceA);
+		var boiteB = new Boite(numberB, nameB, piecesB, figurinesB, priceB);
+		var boiteC = new Boite(numberC, nameC, piecesC, figurinesC, priceC);
+		//then
+		assertTrue(boiteA.compareTo(boiteB)==-1);
+		assertTrue(boiteA.compareTo(boiteC)==1);
+		assertTrue(boiteA.compareTo(boiteA)==0);
+	}
 }
